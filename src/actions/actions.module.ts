24e73fe96e1403d6b91e2828b27bde936b2afe47
Siraj-dev-ui <sirajalig86@gmail.com>
2025-06-23 01:59:29 +0200

    getway injected

--- src/actions/actions.module.ts
+++ src/actions/actions.module.ts
@@ -6,13 +6,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 // import { UsersService } from 'src/users/users.service';
 // import { UsersModule } from 'src/users/users.module';
 import { DoorsModule } from 'src/doors/doors.module';
+import { AppGateway } from 'src/app.gateway';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Action.name, schema: ActionSchema }]),
     DoorsModule,
   ],
-  providers: [ActionsService],
+  providers: [ActionsService, AppGateway],
   controllers: [ActionsController],
 })
 export class ActionsModule {}
